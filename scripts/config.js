94910de4f567e7fddd83a9e76a035e674fc7732b
Adrien <adrienquijo@gmail.com>
2023-10-05 20:30:31 +0200

    first commit

---
+++ scripts/config.js
@@ -0,0 +1,41 @@
+//Liste des mots et phrases utilisés dans le jeu 
+
+const listeMots =  [
+                        "ete",
+                        "etee",
+                        "Rassarin"
+                        ];
+
+const listePhrases=[
+                        "Pas de panique !",
+                        "La vie, l’univers et le reste",
+                        "Merci pour le poisson"
+                        ];
+
+
+const insultes= [
+                "Putain", 
+                "Enfoiré", 
+                "Gredin", 
+                "Mécréan"
+                ];
+
+// Configuration du début de score  
+//let score = 0;
+
+// Configuration du type de jeux (choix des mots à taper)
+const typeMots = "mots";
+const typePhrases = "phrases";
+
+// Configuration de la variable utilisateur
+//// Ne pas toucher ////
+//let motUtilisateur;
+
+/***
+ * Configuration de l'alerte insulte
+ */
+
+const alertInsult = "Surveillez votre langage, les insultes ne sont pas tolérées !" 
+const noAlert = "L'Application pour écrire plus vite!" 
+
+
